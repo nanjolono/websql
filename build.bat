41fee661a04088fe80e90f3874668168cbff1b1d
yu.wang <yu.wang@swiftpass.cn>
2025-01-10 10:16:36 +0800

    init

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-@echo off
-set GOOS=linux
-set GOARCH=arm64
-go build -o websql main.go
+@REM @echo off
+@REM set GOOS=linux
+@REM set GOARCH=arm64
+@REM go build -o websql main.go
+
+ go build -o websql.exe main.go
